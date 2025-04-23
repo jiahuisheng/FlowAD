3827fcdea9647a4231d3f5b6367cee6330e97d23
Jiahui Sheng <111057086+jiahuisheng@users.noreply.github.com>
2025-04-23 14:18:57 +0800

    Add files via upload

---
+++ datasets/__init__.py
@@ -0,0 +1,12 @@
+root = 'data/'
+
+from .power import POWER
+from .gas import GAS
+from .hepmass import HEPMASS
+from .miniboone import MINIBOONE
+from .bsds300 import BSDS300
+from .moons import MOONS
+from .mnist import MNIST
+from .pavia import PAVIA
+from .hu import HU
+from .hyperion import HYPERION
